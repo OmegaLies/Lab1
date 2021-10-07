1c06278f1b2ef7aaaaff1fca719dfc85558ca3d7
OmegaUnder <OmegaUnderWall@gmail.com>
2021-10-07 21:46:28 +0300

    Итог

--- DataItem.cs
+++ DataItem.cs
@@ -23,9 +23,10 @@ namespace Lab1
 
         public override string ToString()
         {
-            return base.ToString();
+            return $"X: {X}, Y:{Y}, Value: {C}, Abs:{C.Magnitude}";
         }
     }
+
     public delegate System.Numerics.Complex FdblComplex(double x, double y);
 
     abstract class V1Data
@@ -75,8 +76,8 @@ namespace Lab1
             int count = 0;
             for (int i = 0; i < nItems; ++i)
             {
-                int x = rnd.Next(-1000, 1000);
-                int y = rnd.Next(-1000, 1000);
+                double x = rnd.Next(-500, 500) / 119;
+                double y = rnd.Next(-500, 500) / 119;
                 DataItem tmp = new(x, y, F(x, y));
                 if (Add(tmp))
                 {
@@ -92,16 +93,16 @@ namespace Lab1
         {
             get
             {
-                System.Numerics.Complex sum = 0;
-                foreach (DataItem i in List) { sum += i.C; }
-                return 0;
+                double sum = 0;
+                foreach (DataItem i in List) { sum += i.C.Magnitude; }
+                return sum / Count;
             }
 
         }
 
         public override string ToString()
         {
-            return $"Type: V1DataList, Name: {base.Str},  Date: {base.Time}, Count {Count} ";
+            return $"Type: V1DataList, Name: {base.Str},  Date: {base.Time}, Count {Count}";
         }
 
         public override string ToLongString(string format)
@@ -117,16 +118,122 @@ namespace Lab1
 
     class V1DataArray : V1Data
     {
+        public int Ox { get; }
+        public int Oy { get; }
+        public double Dx { get; }
+        public double Dy { get; }
+        public System.Numerics.Complex[,] List { get; }
 
+        public V1DataArray(string s, DateTime t) : base(s, t)
+        {
+            List = new System.Numerics.Complex[2, 2];
+        }
+
+        public V1DataArray(string s, DateTime t, int ox, int oy, double dx, double dy, FdblComplex F) : base(s, t)
+        {
+            Ox = ox;
+            Oy = oy;
+            Dx = dx;
+            Dy = dy;
+            List = new System.Numerics.Complex[Ox, Oy];
+            for (int i = 0; i < Ox; ++i)
+            {
+                for (int j = 0; j < Oy; ++j)
+                {
+                    List[i, j] = F(Dx * i, Dy * j);
+                }
+            }
+        }
+
+        public override int Count => Ox * Oy;
+        public override double AverageValue
+        {
+            get
+            {
+                double sum = 0;
+                foreach (System.Numerics.Complex i in List) { sum += i.Magnitude; }
+                return sum / Count;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Type: V1DataArray, Name: {base.Str},  Date: {base.Time}, Count: {Count}, Ox: {Ox}, Oy: {Oy}, Dx: {Dx}, Dy: {Dy}";
+        }
+        public override string ToLongString(string format)
+        {
+            string str = $"Type: V1DataArray, Name: {base.Str},  Date: {base.Time}, Count: {Count}, Ox: {Ox}, Oy: {Oy}, Dx: {Dx.ToString(format)}, Dy: {Dy.ToString(format)}\n";
+            for (int i = 0; i < Ox; ++i)
+            {
+                for (int j = 0; j < Oy; ++j)
+                {
+                    str += $"X: {(Dx * i).ToString(format)}, Y: {(Dy * j).ToString(format)}, Value: {List[i, j].ToString(format)}, Abs: {List[i, j].Magnitude.ToString(format)}\n";
+                }
+            }
+            return str;
+        }
+        public static implicit operator V1DataList(V1DataArray arr)
+        {
+            V1DataList res = new(arr.Str, arr.Time);
+            for (int i = 0; i < arr.Ox; ++i)
+            {
+                for (int j = 0; j < arr.Oy; ++j)
+                {
+                    res.Add(new DataItem(arr.Dx * i, arr.Dy * j, arr.List[i, j]));
+                }
+            }
+            return res;
+        }
     }
 
     class V1MainCollection
     {
-        private List<V1Data> List;
+        private List<V1Data> List = new();
         public int Count => List.Count;
         public V1Data this[int index]
         {
             get => List[index];
+        }
+        public bool Contains(string ID)
+        {
+            if (List == null)
+            {
+                return false;
+            }
+            foreach (V1Data i in List)
+            {
+                if (i.Str == ID)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool Add(V1Data v1Data)
+        {
+            if (!Contains(v1Data.Str))
+            {
+                List.Add(v1Data);
+                return true;
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            string str = "";
+            foreach (V1Data i in List)
+            {
+                str += i.ToString() + " ";
+            }
+            return str;
+        }
+        public string ToLongString(string format)
+        {
+            string str = "";
+            foreach (V1Data i in List)
+            {
+                str += i.ToLongString(format) + " ";
+            }
+            return str;
         }
     }
 }
